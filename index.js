ef98d5bcc25d5517ca7161694fd71c9c7c26228c
Satyasri Varaganti <satyasrib@gmail.com>
2018-10-17 01:18:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ function prependKitten(Arnold) {
   return newKittens
 }
 function removeLastKitten() {
-  var newKittens = kittens.slice(-1)
+  var newKittens = kittens.slice(2)
 return newKittens
 }
